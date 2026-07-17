b1ffdc12f381113edfd6324eb13cb01471c786d7
CPNWestQuest <s6919110046@sau.ac.th>
2026-07-17 14:47:11 +0700

    change

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.CGQ_CONFIG = {
   // หลัง Deploy Google Apps Script ให้คัดลอก Web app URL ที่ลงท้ายด้วย /exec
   // แล้วนำมาวางระหว่างเครื่องหมายคำพูดด้านล่าง
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbxtRKrigRzy-kCPxNrd2BLMvkHEaEUBfPH7luEWNIroNDOr0ubJJS_wkodDVYan69PG/exec",
 
   // true = ถ้าเชื่อม Google Sheets ไม่ได้ ผู้เล่นจะลงทะเบียนไม่ได้
   REQUIRE_ONLINE_REPORTING: true,
